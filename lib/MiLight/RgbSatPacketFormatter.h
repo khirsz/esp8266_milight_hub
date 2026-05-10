1c201fddf47962a56bd55745f8f09797ecef317f
Karol Hirsz <karol.hirsz@gmail.com>
2026-05-10 23:29:09 +0200

    Update rgb_sat with missing light modes

--- lib/MiLight/RgbSatPacketFormatter.h
+++ lib/MiLight/RgbSatPacketFormatter.h
@@ -3,7 +3,7 @@
 #ifndef _RGB_SAT_PACKET_FORMATTER_H
 #define _RGB_SAT_PACKET_FORMATTER_H
 
-#define RGB_SAT_TOTAL_MODES 13
+#define RGB_SAT_TOTAL_MODES 15
 #define RGB_SAT_RESET_HUE   240  // blue, in degrees
 
 struct RgbSatModeMapping {
